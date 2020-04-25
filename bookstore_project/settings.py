3ea8fa42920ca193d7c4cf4ba2ca1d059829f504
c00rv00 <crow7@protonmail.com>
2020-04-25 17:54:07 +0200

    ch4--

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # local
     'users.apps.UsersConfig', # new
+    'pages.apps.PagesConfig'
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'bookstore_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], # new 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,4 +126,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-AUTH_USER_MODEL = 'users.CustomUser' # new 
+AUTH_USER_MODEL = 'users.CustomUser' # new
