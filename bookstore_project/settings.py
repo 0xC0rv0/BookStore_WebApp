add5a814456399beb1538fa94b9cc3abb5a20b6b
ramysaafan17 <crow7@protonmail.com>
2020-04-29 00:27:35 +0200

    static assets

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -38,9 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #Third-party
+    'crispy_forms', # new2
+
     # local
     'users.apps.UsersConfig', # new
     'pages.apps.PagesConfig'
+
+
 ]
 
 MIDDLEWARE = [
@@ -125,7 +130,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'users.CustomUser' # new
 LOGIN_REDIRECT_URL='home'
 LOGOUT_REDIRECT_URL = 'home' # new
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
+
+#django-crispy-forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4' # new 3
